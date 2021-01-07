8645d50e6eb7d8fdbd03d574688c5f6383980049
Kazuki Onishi <princeontrojanhorse@gmail.com>
2021-01-08 00:19:23 +0900

    :bookmark: add version option

--- cmd/dines/main.go
+++ cmd/dines/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
+	"github.com/0n1shi/dines/pkg/disasm"
 	cli "github.com/urfave/cli/v2"
+)
+
+const (
+	Version = "v0.0.1"
 )
 
 func main() {
 	flags := []cli.Flag{
 		&cli.StringFlag{
 			Name:     "rom",
-			Usage:    "file path of NES ROM",
+			Usage:    "A file path of NES ROM",
 			Required: true,
 		},
 	}
 
 	app := cli.App{
-		Name:   "Dines",
-		Usage:  `A disassembler for customed 8-bit microprocessor, "MOS Technology 6502" in Nintendo Entertainment System written in Golang.`,
-		Action: run,
-		Flags:  flags,
+		Name:    "Dines",
+		Usage:   `A disassembler for customed 8-bit microprocessor, "MOS Technology 6502" in Nintendo Entertainment System written in Golang.`,
+		Version: Version,
+		Action:  run,
+		Flags:   flags,
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -31,7 +36,9 @@ func main() {
 
 func run(c *cli.Context) error {
 	filePath := c.String("rom")
-
-	fmt.Printf("file path: %s\n", filePath)
+	_, err := disasm.NewDines(filePath)
+	if err != nil {
+		return err
+	}
 	return nil
 }
